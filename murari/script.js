816311ce9f707807896e842283819c11cdf9db96
murari9472 <murarirai64@gmail.com>
2025-01-27 12:36:53 +0530

    Add files via upload

---
+++ murari/script.js
@@ -0,0 +1,24 @@
+// const pros={
+//     car:"bond",
+//     price: "in Lakh"
+// }
+// let {car} = pros;
+
+// console.log(car);
+
+
+// const pros =["bond", "price"]
+
+// let [car,dear,here] = pros;
+
+// console.log(car,dear,here);
+
+const element = document.querySelector(h1);
+
+if (element) {
+    element.addEventListener("click", () => {
+        element.innerText = "Hi, I'm here";
+    });
+}
+
+
